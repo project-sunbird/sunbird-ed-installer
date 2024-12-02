987f3ad5c92ba54053ed0ad25bcccb63a5e76ed6
divyagovindaiah <divyag@sanketika.in>
2024-12-02 22:21:17 +0530

    00: fix:updated the helm chart global variable reference

--- terraform/azure/modules/keys/variables.tf
+++ terraform/azure/modules/keys/variables.tf
@@ -39,11 +39,4 @@ variable "rsa_keys_count" {
     default     = 2
 }
 
- variable "random_string" {
-    type        = string
-    description = "This string will be used to encrypt / mask various values. Use a strong random string in order to secure the applications. The string should be between 12 and 24 characters in length. If you forget the string, the application will stop working and the string cannot be retrieved."
-    validation {
-      condition     = length(var.random_string) >= 12 || length(var.random_string) <= 24
-      error_message = "The string must have a length ranging from 12 to 24 characters."
-  }
-}
+ 
