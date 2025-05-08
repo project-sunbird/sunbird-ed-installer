a1cd12434842917e513e95f2094d94650a359268
vinodbhorge <vinodbbhorge@gmail.com>
2025-05-08 19:37:23 +0530

    Updated velero deployment and gcp storage module

--- terraform/gcp/modules/storage/main.tf
+++ terraform/gcp/modules/storage/main.tf
@@ -2,18 +2,27 @@ data "google_project" "current" {
   project_id = var.project
 }
 
-locals {
-    common_tags = {
-      environment = "${var.environment}"
-      BuildingBlock = "${var.building_block}"
-    }
-    environment_name = "${var.building_block}-${var.environment}"
+resource "random_id" "bucket_id" {
+  byte_length = 5
 }
+
+locals {
+  unique_uuid = random_id.bucket_id.hex
+
+  common_tags = {
+    environment    = var.environment
+    BuildingBlock  = var.building_block
+    unique_uuid    = local.unique_uuid
+  }
+
+  environment_name = "${var.building_block}-${var.environment}"
+}
+
 resource "google_storage_bucket" "storage_container_public" {
-  project         = var.project
-  name            = "${local.environment_name}-public"
-  location        = var.region
-  force_destroy   = true
+  project       = var.project
+  name          = "${local.environment_name}-public-${local.unique_uuid}"
+  location      = var.region
+  force_destroy = true
 
   versioning {
     enabled = true
@@ -35,11 +44,12 @@ resource "google_storage_bucket_iam_member" "full_access_public" {
   role   = "roles/storage.admin"
   member = "allUsers"
 }
+
 resource "google_storage_bucket" "storage_container_private" {
-  project         = var.project
-  name            = "${local.environment_name}-private"
-  location        = var.region
-  force_destroy   = true
+  project       = var.project
+  name          = "${local.environment_name}-private-${local.unique_uuid}"
+  location      = var.region
+  force_destroy = true
 
   versioning {
     enabled = true
@@ -48,26 +58,25 @@ resource "google_storage_bucket" "storage_container_private" {
   uniform_bucket_level_access = true
 }
 
-
 resource "google_storage_bucket" "dial_state_container_public" {
-  name            = "${local.environment_name}-dial"
-  project         = var.project
-  location        = var.region
-  force_destroy   = true
+  name          = "${local.environment_name}-dial-${local.unique_uuid}"
+  project       = var.project
+  location      = var.region
+  force_destroy = true
 
   versioning {
     enabled = true
   }
 
   uniform_bucket_level_access = false
-cors {
-  origin          = ["https://${var.domain}"]
-  method          = ["GET", "POST", "PUT", "DELETE"]
-  response_header = ["*"]
-  max_age_seconds = 3600
-}
-}
 
+  cors {
+    origin          = ["https://${var.domain}"]
+    method          = ["GET", "POST", "PUT", "DELETE"]
+    response_header = ["*"]
+    max_age_seconds = 3600
+  }
+}
 
 resource "google_storage_bucket_iam_member" "full_access_dial" {
   bucket = google_storage_bucket.dial_state_container_public.name
